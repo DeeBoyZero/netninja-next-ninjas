41de1988117537768e264e5a961f0b81e8469e08
Mathieu Desilets <mathieu.desilets@gmail.com>
2021-10-13 20:28:27 -0400

    finished project

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2021 DBWebdev</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, vero
+          doloribus. Distinctio vero aspernatur ad, recusandae fugiat
+          consequatur corporis, quas quos reiciendis repellat quasi dolorem
+          eligendi quo quidem dolores perspiciatis!
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, vero
+          doloribus. Distinctio vero aspernatur ad, recusandae fugiat
+          consequatur corporis, quas quos reiciendis repellat quasi dolorem
+          eligendi quo quidem dolores perspiciatis!
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, vero
+          doloribus. Distinctio vero aspernatur ad, recusandae fugiat
+          consequatur corporis, quas quos reiciendis repellat quasi dolorem
+          eligendi quo quidem dolores perspiciatis!ßß
+        </p>
+      </div>
+    </>
+  );
+}
